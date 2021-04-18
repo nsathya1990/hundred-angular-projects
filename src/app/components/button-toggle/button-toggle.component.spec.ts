538fde2670de54790ab088d648be84ed2e4c91ec
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-04-18 20:26:46 +0530

    button toggle component testing

--- src/app/components/button-toggle/button-toggle.component.spec.ts
+++ src/app/components/button-toggle/button-toggle.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ButtonMeta } from './button-meta.model';
+
 import { ButtonToggleComponent } from './button-toggle.component';
 
 describe('ButtonToggleComponent', () => {
@@ -8,9 +10,8 @@ describe('ButtonToggleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ButtonToggleComponent ]
-    })
-    .compileComponents();
+      declarations: [ButtonToggleComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +20,45 @@ describe('ButtonToggleComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should exist', () => {
+    expect(ButtonToggleComponent).toBeDefined();
+  });
+
+  describe('defaults', () => {
+    it('options is empty', () => {
+      expect(Array.isArray(component.options)).toBe(true);
+      expect(component.options.length).toBe(0);
+    });
+  });
+
+  describe('selected', () => {
+    it('selects the active option', () => {
+      /** Arrange */
+      component.options = [
+        new ButtonMeta({ title: 'title 1', isActive: true }),
+        new ButtonMeta({ title: 'title 2' }),
+        new ButtonMeta({ title: 'title 3' }),
+      ];
+      /** Act */
+      component.selected(component.options[1]);
+      /** Assertion */
+      expect(component.options[0].isActive).toBe(false);
+      expect(component.options[1].isActive).toBe(true);
+    });
+
+    it('notifies parent of button selection', () => {
+      /** Arrange */
+      component.options = [
+        new ButtonMeta({ title: 'title 1', isActive: true }),
+        new ButtonMeta({ title: 'title 2' }),
+        new ButtonMeta({ title: 'title 3' }),
+      ];
+      component.selection.subscribe((value: ButtonMeta) => {
+        /** Assertion */
+        expect(value).toBe(component.options[2]);
+      });
+      /** Act */
+      component.selected(component.options[2]);
+    });
   });
 });
