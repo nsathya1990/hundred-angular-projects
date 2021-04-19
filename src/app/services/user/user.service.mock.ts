803348c2295411cce0e7146b70d187fb8e93b10c
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-04-19 06:28:57 +0530

    service testing

--- src/app/services/user/user.service.mock.ts
+++ src/app/services/user/user.service.mock.ts
@@ -4,10 +4,12 @@ export class HttpClientMock {
   lastUrl = '';
   lastOptions = null;
   response: { [key: string]: any };
+  lastHttpMethod: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
 
   get(url: string, options?: { [key: string]: any }): Observable<any> {
     this.lastUrl = url;
     this.lastOptions = options;
+    this.lastHttpMethod = 'GET';
 
     return of(this.response);
   }
--- src/app/services/user/user.service.spec.ts
+++ src/app/services/user/user.service.spec.ts
@@ -1,16 +1,58 @@
-import { TestBed } from '@angular/core/testing';
+import { User } from './user.model';
 
 import { UserService } from './user.service';
 
-describe('UserService', () => {
+import { HttpClientMock } from './user.service.mock';
+
+describe(UserService.name, () => {
   let service: UserService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+  it('should exist', () => {
+    let http = new HttpClientMock();
+    service = new UserService(http as any);
+    expect(service).toBeDefined();
   });
 
-  /* it('should be created', () => {
-    expect(service).toBeTruthy();
-  }); */
+  describe('General', () => {
+    let service: UserService;
+    let http: HttpClientMock;
+
+    beforeEach(() => {
+      http = new HttpClientMock();
+      service = new UserService(http as any);
+    });
+
+    describe('getUserById', () => {
+      it('hits correct route with id', async () => {
+        /** arrange */
+        const id = 5;
+        const correctUrl = `http://jsonplaceholder.typicode.com/users/${id}`;
+        http.lastUrl = null;
+        /** act */
+        await service.getUserById(id).toPromise();
+        /** assert */
+        expect(http.lastUrl).toBe(correctUrl);
+      });
+
+      it('is a GET call', async () => {
+        /** arrange */
+        http.lastHttpMethod = null;
+        /** act */
+        await service.getUserById(5).toPromise();
+        /** assert */
+        expect(http.lastHttpMethod).toBe('GET');
+      });
+
+      it('returns a User', async () => {
+        /** arrange */
+        const name = 'Sathya';
+        http.response = { name };
+        /** act */
+        const result = await service.getUserById(5).toPromise();
+        /** assert */
+        expect(result).toBeInstanceOf(User);
+        expect(result.name).toBe(name);
+      });
+    });
+  });
 });
